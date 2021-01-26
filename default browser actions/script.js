8fda290a05a08336570bd32a06e27c16d7ff1a10
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-26 20:39:12 +0200

    ball in field - fixed solution

---
+++ default browser actions/script.js
@@ -0,0 +1,7 @@
+const parentOfLinks = document.querySelectorAll(".paragraph");
+console.log(parentOfLinks);
+parentOfLinks.addEventListener('click', alertQuestion)
+
+function alertQuestion(){
+    
+}
--- introduction browser events/move the ball across the field/script.js
+++ introduction browser events/move the ball across the field/script.js
@@ -2,8 +2,8 @@ let myField = document.getElementById('field');
 let fieldBorder = myField.getBoundingClientRect();
 let myBall = document.getElementById('ball');
 myField.addEventListener('click', (e) =>{
-
     myBall.style.position = 'absolute';
-    myBall.style.left = e.clientX - fieldBorder.clientLeft - myBall.offsetWidth/2;
-    myBall.style.top= e.clientY - fieldBorder.clientTop - myBall.offsetHeight/2;
+    console.log(fieldBorder.clientLeft);
+    myBall.style.left = (e.clientX - fieldBorder.left - myBall.offsetWidth/2) + 'px';
+    myBall.style.top= (e.clientY - fieldBorder.top - myBall.offsetHeight/2) + 'px';
 })
