c16724b340453af1622a0c5b76a9b8bffe80cc32
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-27 19:51:59 +0200

    hidden item - fix
    use hidden insted of delete (like the task asked). using closest method.

--- Event delegation/script.js
+++ Event delegation/script.js
@@ -3,11 +3,13 @@ const container = document.getElementById('container');
 console.log(container);
 
 container.addEventListener('click', (e) => {
-    let para = e.target.closest('button');
-    if(!para) return;
+    let clickedButton = e.target.closest('button');
 
-    if(!container.contains(para)) return;
+    if(!clickedButton) return;
 
-    let selectedItem = e.target.closest('div');
-    container.remove(selectedItem);
+    if(!container.contains(clickedButton)) return;
+
+    let selectedItem = clickedButton.closest('div');
+    console.log(selectedItem);
+    selectedItem.hidden = true;
 })
