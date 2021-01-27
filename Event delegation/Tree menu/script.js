15aa457d288be7ff8b8ba96e6e0104fbb087466f
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-27 20:25:46 +0200

    tree menu solution
    wrok as accepted. Theres an error when we click on a <li> with no children.

---
+++ Event delegation/Tree menu/script.js
@@ -0,0 +1,23 @@
+const treeContainer = document.getElementById("tree");
+
+treeContainer.addEventListener('click', hideChildren);
+
+function hideChildren(e){
+    let clickedTarget = e.target.closest('li');
+ 
+console.log(clickedTarget.hasChildNodes());
+
+    if (clickedTarget.hasChildNodes()) {
+        let children = clickedTarget.childNodes[1];
+        children.hidden = hiddenStatus(children);
+      } else{
+          return;
+      }
+}
+
+function hiddenStatus(trg){  //check hidden status (true/false) and return the opposite
+    if(trg.hidden){
+        return false;
+    }
+    return true;
+}
